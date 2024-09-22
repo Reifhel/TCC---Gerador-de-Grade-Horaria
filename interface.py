fa7b9cf7c91515454fed2fb10e1c34b0effc068b
Reifhel <rafaelherdt1@gmail.com>
2024-09-22 17:16:20 -0300

    (artefato-dois) Ajustes na função de popular tabela e outros pontos

--- interface.py
+++ interface.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import QTimer
 from ui_interface import Ui_MainWindow
 from scheduler import carrega_arquivos, main, PROGRESSO
 
+
 class Interface():
     def __init__(self) -> None:
         self.janela = QMainWindow()
@@ -15,8 +16,8 @@ class Interface():
         self.ui.stackedWidget.setCurrentWidget(self.ui.home)
 
         ''' Botões '''
-        #self.ui.botaoCurso.setEnabled(False)
-        #self.ui.botaoProf.setEnabled(False)
+        # self.ui.botaoCurso.setEnabled(False)
+        # self.ui.botaoProf.setEnabled(False)
 
         # Alternar abas
         self.ui.menuHome.clicked.connect(lambda _, aba=self.ui.home: self.abrirAba(aba))
@@ -34,9 +35,9 @@ class Interface():
 
         # Executar o código
         self.ui.botaoEnsalamento.clicked.connect(self.gerarEnsalamento)
-        self.ui.aplicarFiltrosCurso.clicked.connect(self.popularTabela)
-        self.ui.aplicarFiltrosProf.clicked.connect(self.popularTabela)
-        self.ui.aplicarFiltrosSala.clicked.connect(self.popularTabela)
+        self.ui.aplicarFiltrosCurso.clicked.connect(lambda _, grade="C": self.popularTabela(grade))
+        self.ui.aplicarFiltrosProf.clicked.connect(lambda _, grade="P": self.popularTabela(grade))
+        self.ui.aplicarFiltrosSala.clicked.connect(lambda _, grade="S": self.popularTabela(grade))
 
         # Estrutura padrão da tabela
         larguraColuna = 139
@@ -63,7 +64,7 @@ class Interface():
             self.ui.arquivoProfs.setText(file[0])
             self.arquivoProf = file[0]
 
-    def popularFiltros(self, cursos, periodos, turmas):
+    def popularFiltros(self, cursos, periodos, turmas, professores):
         filtros = {
             self.ui.filtroCurso: "Curso",
             self.ui.filtroCursoProf: "Curso",
@@ -73,7 +74,8 @@ class Interface():
             self.ui.filtroPeriodoSala: "Período",
             self.ui.filtroTurma: "Turma",
             self.ui.filtroTurmaProf: "Turma",
-            self.ui.filtroTurmaSala: "Turma"
+            self.ui.filtroTurmaSala: "Turma",
+            self.ui.filtroProfessor: "Professor"
             # Falta add filtroProfessor e filtroSala
         }
 
@@ -82,49 +84,60 @@ class Interface():
                 filtro.addItems(cursos)
             elif contexto == "Período":
                 filtro.addItems(periodos)
+            elif contexto == "Professor":
+                filtro.addItems(professores)
             else:
                 filtro.addItems(turmas)
 
-    def popularTabela(self):
-        tabelas = {
-            self.ui.tabelaGrade: self.ui.filtroTurma,
-            self.ui.tabelaGradeProfessor: self.ui.filtroTurmaProf,
-            self.ui.tabelaGradeSala: self.ui.filtroTurmaSala
-        }
+    def popularTabela(self, grade):
 
-        for tabela, filtro in tabelas.items():
-            # Limpando conteúdo da tabela
-            tabela.setRowCount(0)
+        if grade == "C":
+            tabela = self.ui.tabelaGrade
+            valor_selecionado = self.ui.filtroTurma.currentText()
+            grade_selecionada = self.turmas.get(valor_selecionado)
 
-            # Selecionando grade de acordo com filtro
-            turma_selecionada = filtro.currentText()
-            grade_selecionada = self.melhor_individuo.get(turma_selecionada)
+        elif grade == "P":
+            tabela = self.ui.tabelaGradeProfessor
+            valor_selecionado = self.ui.filtroProfessor.currentText()
+            grade_selecionada = self.grade_professor.get(valor_selecionado)
 
-            h_cnt = 0
-            for i in range(len(grade_selecionada)):
-                posicao_atual = tabela.rowCount()
-                tabela.insertRow(posicao_atual)
-                hora = self.horarios[h_cnt]
-                intervalo = f"{hora['starttime']:2s} - {hora['endtime']:2s}"
-                intervalo_atual = QTableWidgetItem(intervalo)
-                tabela.setItem(posicao_atual, 0, intervalo_atual)
-                for j in range(len(grade_selecionada[i])):
-                    id = str(grade_selecionada[i][j]) if str(grade_selecionada[i][j]) != "None" else "-"
-                    id_atual = QTableWidgetItem(id)
-                    tabela.setItem(posicao_atual, j+1, id_atual)
-                h_cnt += 1
+        elif grade == "S":
+            tabela = self.ui.tabelaGradeSala
+            valor_selecionado = self.ui.filtroTurmaSala.currentText()
+            grade_selecionada = self.turmas.get(valor_selecionado)
+
+        tabela.setRowCount(0)
+
+        h_cnt = 0
+        for i in range(len(grade_selecionada)):
+            posicao_atual = tabela.rowCount()
+            tabela.insertRow(posicao_atual)
+            hora = self.horarios[h_cnt]
+            intervalo = f"{hora['starttime']:2s} - {hora['endtime']:2s}"
+            intervalo_atual = QTableWidgetItem(intervalo)
+            tabela.setItem(posicao_atual, 0, intervalo_atual)
+
+            for j in range(len(grade_selecionada[i])):
+                id = str(grade_selecionada[i][j]) if str(grade_selecionada[i][j]) != "None" else "-"
+                id_atual = QTableWidgetItem(id)
+                tabela.setItem(posicao_atual, j+1, id_atual)
+
+            h_cnt += 1
+        tabela.verticalHeader().setVisible(False)
 
     def gerarEnsalamento(self):
-        arquivo_dispo_prof = "../data/magister_asctimetables_2024-04-22-15-12-35_curitiba.xml"
-        arquivo_salas = "../Data/Relatorio_dos_Espacos_de_Ensino 1.xlsx"
+        arquivo_dispo_prof = self.arquivoProf
+        arquivo_salas = self.arquivoSalas
         arquivo_turmas = "../Data/politecnica/"
-        arquivo_prof = "../Data/Planilha_Geral_Professores.xlsm"
+        arquivo_prof = self.arquivoTurmas
 
         data, self.horarios = carrega_arquivos(arquivo_dispo_prof, arquivo_salas, arquivo_turmas, arquivo_prof, "2024/2")
-        self.melhor_individuo, _ = main(data, self.horarios)
+        self.turmas, self.grade_professor = main(data, self.horarios)
+        print(self.turmas, self.grade_professor)
         cursos = []
         periodos = []
         turmas = []
+        professores = []
         for id, value in data.turmas.items():
             if value.curso not in cursos and isinstance(value.curso, str):
                 cursos.append(value.curso)
@@ -133,8 +146,11 @@ class Interface():
             if id not in turmas:
                 turmas.append(id)
 
-        self.popularFiltros(cursos, periodos, turmas)
-        self.popularTabela()
+        for id, value in self.grade_professor.items():
+            if id not in professores:
+                professores.append(id)
+
+        self.popularFiltros(cursos, periodos, turmas, professores)
 
 
 if __name__ == '__main__':
